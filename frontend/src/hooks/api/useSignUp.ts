f8d831f9684b695f8d9f098ce9a2e949bfb8e3c2
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-30 14:20:11 -0300

    feat: api connection and post sign up request

---
+++ frontend/src/hooks/api/useSignUp.ts
@@ -0,0 +1,17 @@
+import useAsync from "../useAsync";
+
+import * as userApi from "../../services/api/userApi";
+
+export default function useSignUp() {
+  const {
+    loading: signUpLoading,
+    error: signUpError,
+    act: signUp,
+  } = useAsync(userApi.signUp, { immediate: false });
+
+  return {
+    signUpLoading,
+    signUpError,
+    signUp,
+  };
+}
