b156ff77aba458fbc86ce2e669c51919f48f1fd8
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-30 15:00:02 -0300

    merge changes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import NotFoundPage from "../pages/NotFoundPage";
-import SignUpPage from "../pages/SIgnUpPage";
+import SignUpPage from "../pages/SignUpPage";
+import SignInPage from "../pages/SignInPage";
 
 export const router = createBrowserRouter([
   {
     path: "/cadastro",
     element: <SignUpPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/",
+    element: <SignInPage />,
     errorElement: <NotFoundPage />,
   },
   {
