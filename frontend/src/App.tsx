a90c868a4d3ca5a54cd271ecd0f7b083ad24c917
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-30 08:52:44 -0300

    feat: router provider created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import { ToastContainer } from "react-toastify";
+import { RouterProvider } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+
 import GlobalStyle from "./assets/styles/GlobalStyle";
+import { router } from "./routes";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <GlobalStyle />
+      <RouterProvider router={router} />
     </>
   );
 }
