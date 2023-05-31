1f714b196ac3be49a62875f8ee9d1bab2eeab48d
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-05-31 12:11:09 -0300

    feat: radio input

---
+++ frontend/src/components/Form/RadioInput.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import * as Style from "./style";
+
+interface RadioOption {
+  id: number;
+  label: string;
+}
+
+interface RadioInputProps {
+  label: string;
+  id: string;
+  name: string;
+  value?: number;
+  options: RadioOption[];
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
+  required?: boolean;
+}
+
+export default function RadioInput({
+  label,
+  id,
+  name,
+  value,
+  options,
+  onChange,
+  disabled,
+  required,
+}: RadioInputProps) {
+  return (
+    <Style.Container>
+      <Style.Label>{label}</Style.Label>
+      {options.map((option) => (
+        <Style.RadioContainer key={option.id}>
+          <Style.RadioInput
+            type="radio"
+            id={`${id}_${option.id}`}
+            name={name}
+            value={option.id}
+            checked={option.id === value}
+            onChange={onChange}
+            disabled={disabled}
+            required={required}
+          />
+          <Style.RadioLabel htmlFor={`${id}_${option.id}`}>
+            {option.label}
+          </Style.RadioLabel>
+        </Style.RadioContainer>
+      ))}
+    </Style.Container>
+  );
+}
